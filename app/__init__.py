93843b1c95711e3e727b5a27d8da1030d03cca5c
RobinRheem <roborheems@gmail.com>
2019-02-04 13:02:07 +1300

    Flask project setup

---
+++ app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+# Init flask application
+app = Flask(__name__)
+app.config.from_object('config')
+
+# Init database with SQLAlchemy
+db = SQLAlchemy(app)
+
+# Set error handlers
+@app.errorhandler(404)
+def not_found(error):
+    return 'Not Found', 404
+
+# TODO: Register blueprints
+
+# Setup database
+db.create_all()
+
---
+++ config.py
@@ -0,0 +1,7 @@
+# Set debug runtime environment
+DEBUG = True
+
+# Database connection
+SQLALCHEMY_DATABASE_URI = "mysql://user:password@db:3306/mememime"
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+
