7863a313eea7ffb7e9ffd8dc0e3c9af15fb515fe
RobinRheem <roborheems@gmail.com>
2019-02-08 15:50:06 +0900

    Create object model

---
+++ app/object/models.py
@@ -0,0 +1,17 @@
+from app import db
+
+
+class Object(db.Model):
+    """
+    `objects` table
+    """
+    __tablename__ = 'objects'
+    id = db.Column(db.Integer, primary_key=True)
+    alias = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(50), nullable=False)
+    status = db.Column(db.String(50), nullable=False)
+    created_at = db.Column(db.DateTime,  default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime,  default=db.func.current_timestamp(), 
+            onupdate=db.func.current_timestamp())
+
