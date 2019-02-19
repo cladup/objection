7fe566d8dccd01b87c9d87c59e69b20bad1c3522
RobinRheem <roborheems@gmail.com>
2019-02-19 17:28:08 +0900

    Implement file uploader

--- app/object/controllers.py
+++ app/object/controllers.py
@@ -1,10 +1,39 @@
-from flask import Blueprint
+from flask import Blueprint, request, Response
+from app.util import uploader
 
 
 object_page = Blueprint('objects', __name__)
 
 
+@object_page.route('/objects', methods=['POST'])
+def create():
+    """
+    Upload object to cloud storage
+    """
+    if 'graphic_model' not in request.files:
+        return Response("graphic_model is empty", 400)
+    graphic_model = request.files.get('graphic_model')
+    if graphic_model.filename == '':
+        return Response("No selected file", 400)
+    if not uploader.allowed_file(graphic_model.filename):
+        return Response("File extension not allowed", 400)
+    # Upload and save to database
+    # TODO: Save to database
+    return uploader.upload(graphic_model)
+
+
 @object_page.route('/objects', methods=['GET'])
-def list():
-    return "Hello, World!"
+def get():
+    """
+    FIXME: Create debug purpose file input UI
+    """
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=graphic_model>
+      <input type=submit value=Upload>
+    </form>
+    '''
 
--- app/util/uploader.py
+++ app/util/uploader.py
@@ -1,5 +1,10 @@
+import os
+import datetime
 from google.cloud import storage
 from nanoid import generate
+
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'fbx', 'glb', 'gltf'])
 
 
 def upload(graphic_object):
@@ -7,14 +12,22 @@ def upload(graphic_object):
     Upload object to cloud storage
     In this app, mostly it'll be fbx, glTF, jpeg
     """
-    client = storage.Client()
-    bucket_name = 'graphic-model-store'
-    bucket = client.get_bucket(bucket_name)
+    client = storage.Client.from_service_account_json(os.environ.get('GCP_CREDENTIALS'))
+    bucket = client.get_bucket(os.environ.get('BUCKET'))
     # Nano id(Better uuid) + timestamp
+    timestamp = int(datetime.datetime.now().timestamp() * 10**6)
     destination_blob_name = "{}_{}".format(
-            generate(size=64),
-            str(datetime.datetime.now().timestamp() * 1000))
+            generate(size=32),
+            str(timestamp))
     blob = bucket.blob(destination_blob_name)
-    blob.upload_from_file(graphic_object)
+    blob.upload_from_file(graphic_object, content_type=graphic_object.content_type)
     return blob.name
+
+
+def allowed_file(filename):
+    """
+    Check whether file extension is allowed
+    """
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
