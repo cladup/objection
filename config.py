3e5169ccfcbda0f462c91160daaaac1c72cfbea2
RobinRheem <roborheems@gmail.com>
2019-02-20 21:38:58 +0900

    Setup flask env variables

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ from dotenv import load_dotenv
 load_dotenv(verbose=True)
 
 # Set debug runtime environment
-DEBUG = True
+FLASK_ENV = os.environ.get("FLASK_ENV", "development")
+FLASK_DEBUG = os.environ.get("FLASK_DEBUG", 1)
 
 # Database connection
 SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URI", "mysql://user:password@db:3306/objection")
