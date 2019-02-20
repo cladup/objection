b131c8cc611387e68cf858a4c5fdc7331faed4d1
RobinRheem <roborheems@gmail.com>
2019-02-20 10:26:03 +0900

    Add extension to destination filename

--- app/util/uploader.py
+++ app/util/uploader.py
@@ -2,6 +2,7 @@ import os
 import datetime
 from google.cloud import storage
 from nanoid import generate
+from werkzeug.utils import secure_filename
 
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'fbx', 'glb', 'gltf'])
@@ -14,12 +15,14 @@ def upload(graphic_object):
     """
     client = storage.Client.from_service_account_json(os.environ.get('GCP_CREDENTIALS'))
     bucket = client.get_bucket(os.environ.get('BUCKET'))
-    # Nano id(Better uuid) + timestamp
+    # Nano id(Better uuid) + timestamp + file extension
     timestamp = int(datetime.datetime.now().timestamp() * 10**6)
-    destination_blob_name = "{}_{}".format(
+    destination_blob_name = "{}_{}.{}".format(
             generate(size=32),
-            str(timestamp))
+            str(timestamp),
+            _file_extension(secure_filename(graphic_object.filename)))
     blob = bucket.blob(destination_blob_name)
+    # TODO: Decide content_type for graphic models
     blob.upload_from_file(graphic_object, content_type=graphic_object.content_type)
     return blob.name
 
@@ -29,5 +32,12 @@ def allowed_file(filename):
     Check whether file extension is allowed
     """
     return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+           _file_extension(filename) in ALLOWED_EXTENSIONS
+
+
+def _file_extension(filename):
+    """
+    Get file extension from filename
+    """
+    return filename.rsplit('.', 1)[1].lower()
 
